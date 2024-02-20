26ec94e94ccb7fc0c7e2753653905a44070d3c27
DeadHyperspace <iwannadiehaha@yandex.ru>
2024-02-21 01:41:32 +0300

    Оптимизировали рутинг, теперь мы можем реагировать на HTTP коды и скейлить их если захотим

--- index.php
+++ index.php
@@ -4,12 +4,24 @@ require 'functions.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes =[
+$routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php',
 ];
-
-if(array_key_exists($uri, $routes)){
-    require $routes[$uri];
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abortRequest(404);
+    }
 }
+function abortRequest($httpCode = 404)
+{
+    http_response_code(404);
+    require "views/{$httpCode}.php";
+    die();
+}
+
+routeToController($uri,$routes);
---
+++ views/404.php
@@ -0,0 +1,12 @@
+<?php require("partials/head.php") ?>
+<?php require("partials/nav.php") ?>
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold">Sorry, we could not find the page, go back?</h1>
+            <p class="at-4">
+                <a href="/" class="text-blue underline">Back to home page!</a>
+            </p>
+        </div>
+    </main>
+
+<?php require("partials/footer.php") ?>
