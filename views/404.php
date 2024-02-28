7dfded1e104916ec206ce3294a212603a386fcfa
DeadHyperspace <iwannadiehaha@yandex.ru>
2024-02-28 23:45:47 +0300

    Текст

--- views/404.php
+++ views/404.php
@@ -4,7 +4,7 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <h1 class="text-2xl font-bold">Sorry, we could not find the page, go back?</h1>
             <p class="at-4">
-                <a href="/" class="text-blue underline">Back to home page!</a>
+                <a href="/" class="text-blue underline">Go back to home page!</a>
             </p>
         </div>
     </main>
