3f2b49a254f6560fda0e5f95228ec39f69ab4dfb
DeadHyperspace <iwannadiehaha@yandex.ru>
2024-02-29 00:20:07 +0300

    Сделали PDO

--- index.php
+++ index.php
@@ -2,3 +2,7 @@
 
 require 'functions.php';
 require 'router.php';
+
+//connection to the PostgresSQL DB
+$dsn = "pgsql:host=localhost;port=5432;dbname=demo;user=devwannabe;password=root";
+new PDO($dsn);
